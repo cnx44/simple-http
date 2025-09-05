a764482fd5e740c54558e454685ab3ca05f6f449
Cnx44 <oliverquin2222@gmail.com>
2025-09-05 10:03:03 +0200

    Added server heder filer Added fields in env struct Completed env struct parsing from conf file

---
+++ include/server.h
@@ -0,0 +1,16 @@
+#ifndef SERVER_H 
+#define SERVER_H
+
+
+#define PORT 8080
+#define MESSAGE_SIZE 8192
+#define LINE_SIZE 512
+
+struct environment_var{
+	char* pages_folder;
+	char* index;
+	char* fav_ico;
+	char* error_pages;
+};
+
+#endif
--- src/server.c
+++ src/server.c
@@ -1,3 +1,4 @@
+#include "server.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,11 +6,84 @@
 #include "http_parse.h"
 #include "http_resp.h"
 
-#define PORT 8080
-#define MESSAGE_SIZE 8192
+void enviroment_parser(struct environment_var* environment, char* path){
+	FILE* file_ptr;
+	char line_buffer[LINE_SIZE];
+	char* word_buffer;
+	int word_len;
+	char delim[] = " \t";
+		
+	file_ptr = fopen(path, "r");
+	if(!file_ptr){
+		fprintf(stderr, "Error on conf file opening\n");
+		exit(EXIT_FAILURE);
+	}
 
+	while(fgets(line_buffer, LINE_SIZE, file_ptr)){
+		word_buffer = strtok(line_buffer, delim);	
+		
+		if(!strcmp(word_buffer, "index:")){
+			word_buffer = strtok(NULL, delim); 
+			word_len = strlen(word_buffer) + 1;
 
-int main(){
+			environment->index = malloc(word_len * sizeof(char));
+			if(!environment->index){
+				fprintf(stderr, "Couldn't allocate memory for env-var");
+				exit(EXIT_FAILURE);	
+			}
+
+			strcpy(environment->index, word_buffer);
+			environment->index[word_len] = '\0';
+		}else if(!strcmp(word_buffer, "favico:")){
+			word_buffer = strtok(NULL, delim); 
+			word_len = strlen(word_buffer) + 1;
+
+			environment->fav_ico= malloc(word_len * sizeof(char));
+			if(!environment->fav_ico){
+				fprintf(stderr, "Couldn't allocate memory for env-var");
+				exit(EXIT_FAILURE);	
+			}
+
+			strcpy(environment->fav_ico, word_buffer);
+			environment->fav_ico[word_len] = '\0';
+		}else if(!strcmp(word_buffer, "pages:")){
+			word_buffer = strtok(NULL, delim); 
+			word_len = strlen(word_buffer) + 1;
+
+			environment->pages_folder= malloc(word_len * sizeof(char));
+			if(!environment->pages_folder){
+				fprintf(stderr, "Couldn't allocate memory for env-var");
+				exit(EXIT_FAILURE);	
+			}
+
+			strcpy(environment->pages_folder, word_buffer);
+			environment->pages_folder[word_len] = '\0';
+		}else if(!strcmp(word_buffer, "error_pages:")){
+			word_buffer = strtok(NULL, delim); 
+			word_len = strlen(word_buffer) + 1;
+
+			environment->error_pages = malloc(word_len * sizeof(char));
+			if(!environment->error_pages){
+				fprintf(stderr, "Couldn't allocate memory for env-var");
+				exit(EXIT_FAILURE);	
+			}
+
+			strcpy(environment->error_pages, word_buffer);
+			environment->error_pages[word_len] = '\0';
+		}
+		
+	}
+} 
+
+int main(int argc, char *argv[]){
+	if(argc != 2){
+		fprintf(stderr, "Please provide a conf file\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	struct environment_var env;
+	enviroment_parser(&env, argv[1]);
+
 	int cnt = 0;
 	
 	struct sockaddr_in address;
